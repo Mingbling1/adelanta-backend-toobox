5cdffc3327c9cb670a664009b95396c67867ea4a
Mingbling1 <jimmy.auris@outlook.com>
2025-06-10 20:03:55 -0500

    Migración completa a ECR con GitHub Actions Runner v7

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(apiv2.adelantafactoring.com)
+domains=(apiToolbox.adelantafactoring.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="jimmy.auris@adelantafactoring.com" # Adding a valid address is strongly recommended
