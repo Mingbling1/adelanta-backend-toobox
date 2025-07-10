250d77e1d0d81386ffed05cbb83226eb88331e0c
Mingbling1 <jimmy.auris@outlook.com>
2025-07-10 12:20:36 -0500

    fix(actualizarTablaKPIAcumulado): arreglar acumulado

--- cronjobs/datamart/ActualizarTablaKPIAcumuladoCronjob.py
+++ cronjobs/datamart/ActualizarTablaKPIAcumuladoCronjob.py
@@ -1,42 +1,43 @@
 from cronjobs.BaseCronjob import BaseCronjob
-from utils.timing_decorator import timing_decorator
 from config.container import Container
 from dependency_injector.wiring import inject, Provide
-from services.datamart.KPIAcumuladoService import KPIAcumuladoService
-from services.datamart.TipoCambioService import TipoCambioService
 from utils.adelantafactoring.calculos import KPICalcular
 import pandas as pd
+from repositories.datamart.TipoCambioRepository import TipoCambioRepository
+from repositories.datamart.KPIRepository import KPIRepository
 
 
 class ActualizarTablaKPIAcumuladoCronjob(BaseCronjob):
     def __init__(self):
         super().__init__(description="Actualiza la tabla de KPI acumulado")
 
-    @timing_decorator
-    async def actualizar_tabla(self, data: dict, service):
-        await service.delete_all()
-        if data:
-            await service.create_many(data)
-
     @inject
     async def run(
         self,
-        kpi_acumulado_service: KPIAcumuladoService = Provide[
-            Container.kpi_acumulado_service
+        tipo_cambio_repository: TipoCambioRepository = Provide[
+            Container.tipo_cambio_repository
         ],
-        tipo_cambio_service: TipoCambioService = Provide[Container.tipo_cambio_service],
+        kpi_repository: KPIRepository = Provide[Container.kpi_repository],
     ):
-        tipo_cambio_data = await tipo_cambio_service.get_all(limit=None)
-        tipo_cambio_df = pd.DataFrame([i.__dict__ for i in tipo_cambio_data])
-        tipo_cambio_df["TipoCambioFecha"] = pd.to_datetime(
-            tipo_cambio_df["TipoCambioFecha"]
-        )
+        try:
+            # TipoCambio
+            tipo_cambio_df = pd.DataFrame(
+                await tipo_cambio_repository.get_all_dicts(exclude_pk=True)
+            )
+            tipo_cambio_df["TipoCambioFecha"] = pd.to_datetime(
+                tipo_cambio_df["TipoCambioFecha"]
+            )
 
-        kpi_acumulado_calcular = await KPICalcular(tipo_cambio_df).calcular(
-            start_date=BaseCronjob.obtener_datetime_fecha_inicio(),
-            end_date=BaseCronjob.obtener_datetime_fecha_fin(),
-            fecha_corte=BaseCronjob.obtener_datetime_fecha_fin(),
-            tipo_reporte=0,
-        )
+            # KPI Acumulado
+            kpi_acumulado_calcular = await KPICalcular(tipo_cambio_df).calcular(
+                start_date=BaseCronjob.obtener_datetime_fecha_inicio(),
+                end_date=BaseCronjob.obtener_datetime_fecha_fin(),
+                fecha_corte=BaseCronjob.obtener_datetime_fecha_fin(),
+                tipo_reporte=0,
+            )
 
-        await self.actualizar_tabla(kpi_acumulado_calcular, kpi_acumulado_service)
+            await kpi_repository.delete_all()
+            await kpi_repository.create_many(kpi_acumulado_calcular)
+
+        except Exception as e:
+            raise e
